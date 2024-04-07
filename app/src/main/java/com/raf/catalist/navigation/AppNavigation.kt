548f83f30546a137bcee3871df7b3abf5172af0a
Mehmedalija <rg.mehmedalija@gmail.com>
2024-04-07 20:09:21 +0200

    remove imports

--- app/src/main/java/com/raf/catalist/navigation/AppNavigation.kt
+++ app/src/main/java/com/raf/catalist/navigation/AppNavigation.kt
@@ -3,11 +3,8 @@ package com.raf.catalist.navigation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.raf.catalist.cats.details.BreedDetailScreen
 import com.raf.catalist.cats.details.breedDetails
-import com.raf.catalist.cats.list.BreedsListScreen
 import com.raf.catalist.cats.list.breedsListScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
